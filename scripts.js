b888c47e1fc998d120ffdd0bbb4388537c074603
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-08-04 20:11:31 -0300

    desafio 2 terminado...

--- scripts.js
+++ scripts.js
@@ -1,64 +1,59 @@
 //desafio 2
-function preguntas( pregunta1, pregunta2, pregunta3, pregunta4, pregunta5){
+alert("Responde las siguientes preguntas :")
 
-    alert("Responde las siguientes preguntas :")
-
-    pregunta1 = prompt("¿Capital de España?").toLowerCase()
-    if (pregunta1 == "madrid"){
-        num1 = 1
+let pregunta1 = prompt("¿Capital de España?").toLowerCase()
+if (pregunta1 == "madrid"){
+    num1 = 1
     alert("correcto")
-    }
-    else{
-        num1 = 0;
+}
+else{        num1 = 0;
     alert("incorrecto")
     }
 
-    pregunta2 = prompt("¿Capital de Japón?").toLowerCase()
+let pregunta2 = prompt("¿Capital de Japón?").toLowerCase()
     if (pregunta2 == "tokio"){
         num2 = 1;
-    alert("correcto")
+        alert("correcto")
     }
     else{
         num2 = 0;
     alert("incorrecto")
     }
 
-    pregunta3 = prompt("¿Capital de Italia?").toLowerCase()
+let pregunta3 = prompt("¿Capital de Italia?").toLowerCase()
     if (pregunta3 == "roma"){
         num3 = 1;
-    alert("correcto")
+        alert("correcto")
     }
     else{
         num3 = 0;
-    alert("incorrecto")
+        alert("incorrecto")
     }
 
-    pregunta4 = prompt("¿Capital de Brasil?").toLowerCase()
+let pregunta4 = prompt("¿Capital de Brasil?").toLowerCase()
     if (pregunta4 == "brasilia"){
         num4 = 1;
-    alert("correcto")
+        alert("correcto")
     }
     else{
         num4 = 0;
-    alert("incorrecto")
+        alert("incorrecto")
     }
 
-    pregunta5 = prompt("¿Capital de Uruguay?").toLowerCase()
+let pregunta5 = prompt("¿Capital de Uruguay?").toLowerCase()
     if (pregunta5 == "montevideo"){
         num5 = 1;
-    alert("correcto")
+        alert("correcto")
     }
     else{
         num5 = 0;
-    alert("incorrecto")
+        alert("incorrecto")
     }
 
-
-    newPreguntas()
-    function newPreguntas() {
-        puntos = num1 + num2 + num3 + num4 + num5;
-        alert(`Puntos obtenidos : ${puntos}`)
-    }
+//pequeña funcion para calcular el porcentaje de preguntas acertadas
+function puntaje() {
+    let puntos = num1 + num2 + num3 + num4 + num5;
+    let puntaje = puntos * 100 / 5
+    alert(`El porcentaje de aciertos es del %${puntaje}`)
 }
-
-preguntas()
+puntaje()
