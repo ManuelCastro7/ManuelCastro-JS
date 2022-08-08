a4bbb92aa510bc0d6f2f51a3f798255d756d46ee
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-08-08 15:23:40 -0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Desafio 5
 
-let repetir = true
+/*let repetir = true
 
 let nombre = prompt("Ingrese su nombre.").toLowerCase(); 
 
@@ -17,7 +17,7 @@ while (repetir) {
 
  const usuario = document.getElementById("usuario")
  usuario.innerText = `Hola ${nombre}, aqui tiene las mejores computadoras para vos`
-
+*/
 class Computadora{
     constructor(precio, procesador, ram, almacenamiento, os){
         this.procesador = procesador;
@@ -31,8 +31,8 @@ class Computadora{
 //le agrego caracteristicas a cada computadora y luego lo muestro a traves de la consola...
 const computadora1 = new Computadora("15000", "Intel core2 2.40ghz", "2gb ddr3", "240gb hdd", "Windows 7");
 const computadora2 = new Computadora("20000", "AMD athlon 3.0ghz", "4gb ddr3", "500gb hdd", "Windows 8");
-const computadora3 = new Computadora("28500", "Intel i3 3.30ghz", "8gb ddr3", "240gb ssd", "Windows 10");
-const computadora4 = new Computadora("35500", "Intel i5 4.1ghz", "8gb ddr3", "240gb sdd", "Windows 10");
+const computadora3 = new Computadora("28500", "Intel core i3 3.30ghz", "8gb ddr3", "240gb ssd", "Windows 10");
+const computadora4 = new Computadora("35500", "Intel core i5 4.1ghz", "8gb ddr3", "240gb sdd", "Windows 10");
 const computadora5 = new Computadora("42500", "AMD Ryzen3 4.0ghz", "8gb ddr4", "500gb ssd", "Windows 10");
 const computadora6 = new Computadora("55000", "AMD Ryzen5 4.2ghz", "16gb ddr4", "500gb sdd", "Windows 11");
 
@@ -48,6 +48,11 @@ computadoras.forEach(producto => {
         <h3 class="card">Almacenamiento: ${producto.almacenamiento}</h3>
         <h3 class="card">OS: ${producto.os}</h3>
         <h3 class="card">Precio: ${producto.precio}</h3>
+        <button class="comprar" id="boton1">Comprar</button>
     </div>
     `
-});
+})
+
+boton1.addEventListener('click', () => {
+    alert("¿Estas seguro?")
+})
