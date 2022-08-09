f595330410e1e47bdf9705d761ad5b168d5cba81
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-08-09 16:57:41 -0300

    desafio 6

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-//Desafio 6
+let products = [];
+let total = 0;
 
+function add(producto, precio) {
+    console.log(producto, precio);
+    products.push(producto);
+    total = total + precio;
+    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+}
+
+function pay() {
+    window.alert(products.join(", \n"));
+}
+
+//Desafio 6
 
 class Computadora{
     constructor(precio, procesador, ram, almacenamiento, os){
@@ -18,27 +31,25 @@ const computadora3 = new Computadora("28500", "Intel core i3 3.30ghz", "8gb ddr3
 const computadora4 = new Computadora("35500", "Intel core i5 4.1ghz", "8gb ddr3", "240gb sdd", "Windows 10");
 const computadora5 = new Computadora("42500", "AMD Ryzen3 4.0ghz", "8gb ddr4", "500gb ssd", "Windows 10");
 const computadora6 = new Computadora("55000", "AMD Ryzen5 4.2ghz", "16gb ddr4", "500gb sdd", "Windows 11");
-
 const computadoras = [computadora1, computadora2, computadora3, computadora4, computadora5, computadora6]
 
-const divProductos = document.getElementById("divProductos")
-
-computadoras.forEach(producto => {
-    divProductos.innerHTML += `
-    <div class="cards">
-        <h3 class="card">Procesador: ${producto.procesador}</h3>
-        <h3 class="card">Ram: ${producto.ram}</h3>
-        <h3 class="card">Almacenamiento: ${producto.almacenamiento}</h3>
-        <h3 class="card">OS: ${producto.os}</h3>
-        <h3 class="card">Precio: ${producto.precio}</h3>
-        <button id="boton1">COMPRAR</button>
-    </div>
-    `
-})
-
 boton1.addEventListener('click', () => {
-    alert("Compra finalizada")
-    
+    window.alert(computadora1)
+})
+boton2.addEventListener('click', () => {
+    console.table(computadora2)
+})
+boton3.addEventListener('click', () => {
+    console.table(computadora3)
+})
+boton4.addEventListener('click', () => {
+    console.table(computadora4)
+})
+boton5.addEventListener('click', () => {
+    console.table(computadora5)
+})
+boton6.addEventListener('click', () => {
+    console.table(computadora6)
 })
 
 let repetir = true
