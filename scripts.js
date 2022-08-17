ae59856bb790ca945a13a9a6f281bd3d039b65cb
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-08-17 10:37:44 -0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,34 +1,139 @@
-//Desafio 8 pequeño cuestionario
+const Clickbutton = document.querySelectorAll('.button')
+const tbody = document.querySelector('.tbody')
+let carrito = []
 
-alert("Responde las siguientes preguntas :")
+Clickbutton.forEach(btn => {
+  btn.addEventListener('click', addToCarritoItem)
+})
 
-let pregunta1 = prompt("¿Capital de España?").toLowerCase()
-pregunta1 == "madrid" ? alert("correcto", num1 = 1) : alert("incorrecto", num1 = 0);
+
+function addToCarritoItem(e){
+  const button = e.target
+  const item = button.closest('.card')
+  const itemTitle = item.querySelector('.card-title').textContent;
+  const itemPrice = item.querySelector('.precio').textContent;
+  const itemImg = item.querySelector('.card-img-top').src;
+  
+  const newItem = {
+    title: itemTitle,
+    precio: itemPrice,
+    img: itemImg,
+    cantidad: 1
+  }
+
+  addItemCarrito(newItem)
+}
+
+
+function addItemCarrito(newItem){
+
+  const alert = document.querySelector('.alert')
+
+  setTimeout( function(){
+    alert.classList.add('hide')
+  }, 2000)
+    alert.classList.remove('hide')
+
+  const InputElemnto = tbody.getElementsByClassName('input__elemento')
+  for(let i =0; i < carrito.length ; i++){
+    if(carrito[i].title.trim() === newItem.title.trim()){
+      carrito[i].cantidad ++;
+      const inputValue = InputElemnto[i]
+      inputValue.value++;
+      CarritoTotal()
+      return null;
+    }
+  }
+  
+  carrito.push(newItem)
+  
+  renderCarrito()
+} 
+
+
+function renderCarrito(){
+  tbody.innerHTML = ''
+  carrito.map(item => {
+    const tr = document.createElement('tr')
+    tr.classList.add('ItemCarrito')
+    const Content = `
     
-let pregunta2 = prompt("¿Capital de Japón?").toLowerCase()
-pregunta2 == "tokio" ? alert("correcto", num2 = 1) : alert("incorrecto", num2 = 0)
+    <th scope="row">1</th>
+            <td class="table__productos">
+              <img src=${item.img}  alt="">
+              <h6 class="title">${item.title}</h6>
+            </td>
+            <td class="table__price"><p>${item.precio}</p></td>
+            <td class="table__cantidad">
+              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+              <button class="delete btn btn-danger">x</button>
+            </td>
+    
+    `
+    tr.innerHTML = Content;
+    tbody.append(tr)
 
-let pregunta3 = prompt("¿Capital de Italia?").toLowerCase()
-pregunta3 == "roma" ? alert("correcto", num3 = 1) : alert("incorrecto", num3 = 0);
-
-let pregunta4 = prompt("¿Capital de Brasil?").toLowerCase()
-pregunta4 == "brasilia" ? alert("correcto", num4 = 1) : alert("incorrecto", num4 = 0);
-
-let pregunta5 = prompt("¿Capital de Uruguay?").toLowerCase()
-pregunta5 == "montevideo" ? alert("correcto", num5 = 1) : alert("incorrecto", num5 = 0);
-
-//pequeña funcion para calcular el porcentaje de preguntas acertadas
-function puntaje() {
-    let puntos = num1 + num2 + num3 + num4 + num5;
-    let puntaje = puntos * 100 / 5
-    document.write(`El porcentaje de aciertos es del %${puntaje}`)
-}
-puntaje()
-
-const usuario = {
-    nombre : prompt("Ingrese su nombre"),
-    edad : prompt("Ingrese su edad")
+    tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+    tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+  })
+  CarritoTotal()
 }
 
-const ingreso = usuario.edad >= 18 && usuario
-console.log(ingreso)
+function CarritoTotal(){
+  let Total = 0;
+  const itemCartTotal = document.querySelector('.itemCartTotal')
+  carrito.forEach((item) => {
+    const precio = Number(item.precio.replace("$", ''))
+    Total = Total + precio*item.cantidad
+  })
+
+  itemCartTotal.innerHTML = `Total $${Total}`
+  addLocalStorage()
+}
+
+function removeItemCarrito(e){
+  const buttonDelete = e.target
+  const tr = buttonDelete.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  for(let i=0; i<carrito.length ; i++){
+
+    if(carrito[i].title.trim() === title.trim()){
+      carrito.splice(i, 1)
+    }
+  }
+
+  const alert = document.querySelector('.remove')
+
+  setTimeout( function(){
+    alert.classList.add('remove')
+  }, 2000)
+    alert.classList.remove('remove')
+
+  tr.remove()
+  CarritoTotal()
+}
+
+function sumaCantidad(e){
+  const sumaInput  = e.target
+  const tr = sumaInput.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  carrito.forEach(item => {
+    if(item.title.trim() === title){
+      sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
+      item.cantidad = sumaInput.value;
+      CarritoTotal()
+    }
+  })
+}
+
+function addLocalStorage(){
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem('carrito'));
+  if(storage){
+    carrito = storage;
+    renderCarrito()
+  }
+}
