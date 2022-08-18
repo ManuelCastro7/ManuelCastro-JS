40e0cf1621d1cea679a87253efd2efeee5af1a27
Manuel Castro <manuel.castro.ofc@gmail.com>
2022-08-18 16:46:23 -0300

    desafio 9

--- scripts.js
+++ scripts.js
@@ -1,58 +1,59 @@
+// Desafio de librerias
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Variables
   const baseDeDatos = [{
       id: 1,
-      nombre: 'Patata',
-      precio: 1,
-      imagen: 'pc1.png'
+      nombre: "Intel i3",
+      caracteriticas: "2 nucleos, 4 hilos",
+      precio: 5000,
+      imagen: "/img/proce1.jfif"
     },
     {
       id: 2,
-      nombre: 'Cebolla',
-      precio: 1.2,
-      imagen: 'cebolla.jpg'
+      nombre: "Intel I5",
+      caracteriticas: "4 nucleos 4 hilos",
+      precio: 10000,
+      imagen: "/img/proce2.jfif"
     },
     {
       id: 3,
-      nombre: 'Calabacin',
-      precio: 2.1,
-      imagen: 'calabacin.jpg'
+      nombre: "Intel I7",
+      caracteriticas: "4 nucleos 8 hilos",
+      precio: 15000,
+      imagen: "/img/proce3.jfif"
     },
     {
       id: 4,
-      nombre: 'Fresas',
-      precio: 0.6,
-      imagen: 'fresas.jpg'
+      nombre: "Ram 2gb",
+      caracteriticas: "ddr4 1600mhz",
+      precio: 4000,
+      imagen: "/img/ram.jpg"
     },
     {
       id: 5,
-      nombre: 'Fresas',
-      precio: 0.6,
-      imagen: 'fresas.jpg'
+      nombre: "Ram 4gb",
+      caracteriticas: "ddr4 2600mhz",
+      precio: 6000,
+      imagen: "/img/ram.jpg"
     },
     {
       id: 6,
-      nombre: 'Fresas',
-      precio: 0.6,
-      imagen: 'pcgamer.png'
+      nombre: "Ram 8gb",
+      caracteriticas: "ddr4 3600mhz",
+      precio: 8000,
+      imagen: "/img/ram.jpg"
     }
 
   ];
 
   let carrito = [];
-  const divisa = '€';
+  const divisa = '$';
   const DOMitems = document.querySelector('#items');
   const DOMcarrito = document.querySelector('#carrito');
   const DOMtotal = document.querySelector('#total');
   const DOMbotonVaciar = document.querySelector('#boton-vaciar');
   const miLocalStorage = window.localStorage;
 
-  // Funciones
-
-  /**
-   * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
-   */
   function renderizarProductos() {
     baseDeDatos.forEach((info) => {
       // Estructura
@@ -62,26 +63,31 @@ document.addEventListener('DOMContentLoaded', () => {
       const miNodoCardBody = document.createElement('div');
       miNodoCardBody.classList.add('card-body');
       // Titulo
-      const miNodoTitle = document.createElement('h5');
-      miNodoTitle.classList.add('card-title');
-      miNodoTitle.textContent = info.nombre;
+      const miNodoTitulo = document.createElement('h3');
+      miNodoTitulo.classList.add('card-title');
+      miNodoTitulo.textContent = info.nombre;
+      // Caracteristicas
+      const miNodoCaracteristica = document.createElement('h5');
+      miNodoCaracteristica.classList.add('card-title');
+      miNodoCaracteristica.textContent = info.caracteriticas;
       // Imagen
       const miNodoImagen = document.createElement('img');
       miNodoImagen.classList.add('img-fluid');
       miNodoImagen.setAttribute('src', info.imagen);
       // Precio
-      const miNodoPrecio = document.createElement('p');
+      const miNodoPrecio = document.createElement('h5');
       miNodoPrecio.classList.add('card-text');
-      miNodoPrecio.textContent = `${info.precio}${divisa}`;
+      miNodoPrecio.textContent = `${divisa}${info.precio}`;
       // Boton 
       const miNodoBoton = document.createElement('button');
       miNodoBoton.classList.add('btn', 'btn-primary');
-      miNodoBoton.textContent = '+';
+      miNodoBoton.textContent = 'Comprar';
       miNodoBoton.setAttribute('marcador', info.id);
-      miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+      miNodoBoton.addEventListener('click', aniadirProductoAlCarrito);
       // Insertamos
       miNodoCardBody.appendChild(miNodoImagen);
-      miNodoCardBody.appendChild(miNodoTitle);
+      miNodoCardBody.appendChild(miNodoTitulo);
+      miNodoCardBody.appendChild(miNodoCaracteristica);
       miNodoCardBody.appendChild(miNodoPrecio);
       miNodoCardBody.appendChild(miNodoBoton);
       miNodo.appendChild(miNodoCardBody);
@@ -89,42 +95,29 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  /**
-   * Evento para añadir un producto al carrito de la compra
-   */
-  function anyadirProductoAlCarrito(evento) {
-    // Anyadimos el Nodo a nuestro carrito
+  // Añadir un producto al carrito
+  function aniadirProductoAlCarrito(evento) {
     carrito.push(evento.target.getAttribute('marcador'))
-    // Actualizamos el carrito 
     renderizarCarrito();
-    // Actualizamos el LocalStorage
     guardarCarritoEnLocalStorage();
   }
 
-  /**
-   * Dibuja todos los productos guardados en el carrito
-   */
   function renderizarCarrito() {
-    // Vaciamos todo el html
     DOMcarrito.textContent = '';
-    // Quitamos los duplicados
     const carritoSinDuplicados = [...new Set(carrito)];
-    // Generamos los Nodos a partir de carrito
     carritoSinDuplicados.forEach((item) => {
-      // Obtenemos el item que necesitamos de la variable base de datos
       const miItem = baseDeDatos.filter((itemBaseDatos) => {
-        // ¿Coincide las id? Solo puede existir un caso
         return itemBaseDatos.id === parseInt(item);
       });
-      // Cuenta el número de veces que se repite el producto
+
       const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-        // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
         return itemId === item ? total += 1 : total;
       }, 0);
-      // Creamos el nodo del item del carrito
+
       const miNodo = document.createElement('li');
       miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-      miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+      miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${divisa}${miItem[0].precio}`
+
       // Boton de borrar
       const miBoton = document.createElement('button');
       miBoton.classList.add('btn', 'btn-danger', 'mx-5');
@@ -132,21 +125,16 @@ document.addEventListener('DOMContentLoaded', () => {
       miBoton.style.marginLeft = '1rem';
       miBoton.dataset.item = item;
       miBoton.addEventListener('click', borrarItemCarrito);
-      // Mezclamos nodos
       miNodo.appendChild(miBoton);
       DOMcarrito.appendChild(miNodo);
     });
-    // Renderizamos el precio total en el HTML
     DOMtotal.textContent = calcularTotal();
   }
 
-  /**
-   * Evento para borrar un elemento del carrito
-   */
+  // Eliminar un producto del carrito
   function borrarItemCarrito(evento) {
-    // Obtenemos el producto ID que hay en el boton pulsado
     const id = evento.target.dataset.item;
-    // Borramos todos los productos
+    // Eliminar todos los productos
     carrito = carrito.filter((carritoId) => {
       return carritoId !== id;
     });
@@ -155,32 +143,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-  /**
-   * Calcula el precio total teniendo en cuenta los productos repetidos
-   */
+  // Calcula el precio total
   function calcularTotal() {
-    // Recorremos el array del carrito 
     return carrito.reduce((total, item) => {
-      // De cada elemento obtenemos su precio
       const miItem = baseDeDatos.filter((itemBaseDatos) => {
         return itemBaseDatos.id === parseInt(item);
       });
-      // Los sumamos al total
       return total + miItem[0].precio;
     }, 0).toFixed(2);
   }
 
-  /**
-   * Varia el carrito y vuelve a dibujarlo
-   */
   function vaciarCarrito() {
-    // Limpiamos los productos guardados
     carrito = [];
-    // Renderizamos los cambios
     renderizarCarrito();
-    // Borra LocalStorage
     localStorage.clear();
-
   }
 
   function guardarCarritoEnLocalStorage() {
@@ -188,18 +164,39 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function cargarCarritoDeLocalStorage() {
-    // ¿Existe un carrito previo guardado en LocalStorage?
     if (miLocalStorage.getItem('carrito') !== null) {
-      // Carga la información
       carrito = JSON.parse(miLocalStorage.getItem('carrito'));
     }
   }
 
-  // Eventos
   DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 
-  // Inicio
   cargarCarritoDeLocalStorage();
   renderizarProductos();
   renderizarCarrito();
 });
+
+
+
+//Formulario extraido de Developer Mozilla
+const form = document.querySelector('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const para = document.querySelector('h6');
+
+form.addEventListener('submit', (e) => {
+  if (fname.value === '' || lname.value === '') {
+    e.preventDefault();
+    para.textContent = 'Complete los dos campos';
+  }
+});
+
+  const boton = document.getElementById("boton")
+  boton.addEventListener("click" , () =>{
+    Swal.fire(
+      'The Internet?',
+      'That thing is still around?',
+      'question'
+    )
+ })
+
